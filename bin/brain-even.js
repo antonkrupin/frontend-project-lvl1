d011619e2ff6b1115b1f1ab0c281aa90ed26e684
Anton Krupin <krupin_anton@mail.ru>
2021-08-20 17:03:56 +0300

    add actions for corretc yes and no answers

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -13,7 +13,12 @@ while (rightAnswerCounter <= 3) {
     console.log(randomNumber);
     const answer = takeUserInput();
     if (answer === 'yes' && randomNumber % 2 === 0) {
-        console.log('terts');
+        console.log('Your answer: yes');
+        console.log('Correct!');
+        rightAnswerCounter += 1;
+    } else if (answer === 'no' && randomNumber % 2 !== 0) {
+        console.log('Your answer: no');
+        console.log('Correct!');
         rightAnswerCounter += 1;
     }
 }
