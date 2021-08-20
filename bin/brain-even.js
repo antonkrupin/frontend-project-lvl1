5135ec586a55b3ba30be53c814a148e0d5db4cf6
Anton Krupin <krupin_anton@mail.ru>
2021-08-20 18:15:38 +0300

    change answer check logic

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,24 +1,27 @@
 #!/usr/bin/env node
 import takeUserInput from '../src/cli.js';
 
-let rightAnswerCounter = 0;
 const getRandom = (max) => Math.floor(Math.random() * max);
 
 console.log('Welcome to the Brain Games!');
 console.log(`Hello, ${takeUserInput('May I have your name? ')}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-while (rightAnswerCounter <= 3) {
+let rightAnswerCounter = 0;
+while (rightAnswerCounter < 3) {
     const randomNumber = getRandom(1000);
-    console.log(randomNumber);
+    console.log(`Question: ${randomNumber}`);
     const answer = takeUserInput();
-    if (answer === 'yes' && randomNumber % 2 === 0) {
-        console.log('Your answer: yes');
+    const divisionRemainder = randomNumber % 2;
+
+    if ((answer === 'yes' && divisionRemainder === 0) || (answer === 'no' && divisionRemainder !== 0)) {
+        console.log(`Your answer: ${answer}`);
         console.log('Correct!');
         rightAnswerCounter += 1;
-    } else if (answer === 'no' && randomNumber % 2 !== 0) {
-        console.log('Your answer: no');
-        console.log('Correct!');
-        rightAnswerCounter += 1;
+    }
+
+    if ((answer === 'no' && divisionRemainder === 0) || (answer === 'yes' && divisionRemainder !== 0)) {
+        console.log(`${answer} is wrong answer ;(. Correct was another`);
+        break;
     }
 }
