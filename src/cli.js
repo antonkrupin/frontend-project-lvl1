0022c161216959f255010e1818f9a31d9eb35c47
Anton Krupin <krupin_anton@mail.ru>
2021-08-20 14:00:44 +0300

    remove 'export' in function declaration

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const takeUserInput = () => {
+const takeUserInput = () => {
     const input = readlineSync.question('May I have your name? ');
     return input;
 };
