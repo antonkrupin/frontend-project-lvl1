66039bbe3e5d3c8a2b3805b0ecc131c7c419601e
Anton Krupin <krupin_anton@mail.ru>
2021-08-20 13:55:51 +0300

    format 'Hello' greeting

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,4 +3,4 @@ import readlineSync from 'readline-sync';
 import takeUserInput from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-console.log(`Hello ${takeUserInput()}`);
+console.log(`Hello, ${takeUserInput()}!`);
