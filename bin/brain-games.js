3e70a68086232a69b965b32ed3badf35810b93d7
Anton Krupin <krupin_anton@mail.ru>
2021-08-20 13:54:10 +0300

    use takeUserInputFunction

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
+import takeUserInput from '../src/cli.js';
 
 console.log('Welcome to the Brain Games!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello ${name}`);
-
+console.log(`Hello ${takeUserInput()}`);
