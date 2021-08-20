317235cae3ef66213d3791b8f5235eef9e8fda07
Anton Krupin <krupin_anton@mail.ru>
2021-08-20 17:01:22 +0300

    add while loop

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,19 @@
 #!/usr/bin/env node
 import takeUserInput from '../src/cli.js';
 
+let rightAnswerCounter = 0;
 const getRandom = (max) => Math.floor(Math.random() * max);
 
 console.log('Welcome to the Brain Games!');
 console.log(`Hello, ${takeUserInput()}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
-console.log(getRandom(1000));
+
+while (rightAnswerCounter <= 3) {
+    const randomNumber = getRandom(1000);
+    console.log(randomNumber);
+    const answer = takeUserInput();
+    if (answer === 'yes' && randomNumber % 2 === 0) {
+        console.log('terts');
+        rightAnswerCounter += 1;
+    }
+}
