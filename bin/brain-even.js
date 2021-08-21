9809a71027260b150cead6961c7cdbfca01b68d0
Anton Krupin <krupin_anton@mail.ru>
2021-08-21 11:21:03 +0300

    change logic, add isRightAnswer

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -9,31 +9,40 @@ console.log(`Hello, ${userName}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 let rightAnswerCounter = 0;
-while (rightAnswerCounter < 3) {
+let isRightAnswer = true;
+
+while (isRightAnswer) {
     const randomNumber = getRandom(1000);
     console.log(`Question: ${randomNumber}`);
     const answer = takeUserInput();
     const divisionRemainder = randomNumber % 2;
 
     if (answer !== 'yes' || answer !== 'no') {
-        break;
+        isRightAnswer = false;
     }
 
-    if ((answer === 'no' && divisionRemainder === 0) || (answer === 'yes' && divisionRemainder !== 0)) {
+    if ((answer === 'no' && divisionRemainder === 0)
+        || (answer === 'yes' && divisionRemainder !== 0)) {
         if (answer === 'yes') {
             console.log(`${answer} is wrong answer ;(. Correct was 'no'`);
             console.log(`Let\`s try again, ${userName}`);
-            break;
+            isRightAnswer = false;
         }
         console.log(`${answer} is wrong answer ;(. Correct was 'yes'`);
         console.log(`Let\`s try again, ${userName}`);
-        break;
+        isRightAnswer = false;
     }
 
-    if ((answer === 'yes' && divisionRemainder === 0) || (answer === 'no' && divisionRemainder !== 0)) {
+    if ((answer === 'yes' && divisionRemainder === 0)
+        || (answer === 'no' && divisionRemainder !== 0)) {
         console.log(`Your answer: ${answer}`);
         console.log('Correct!');
         rightAnswerCounter += 1;
+        isRightAnswer = true;
+    }
+
+    if (rightAnswerCounter === 3) {
+        console.log(`Congratulations, ${userName}`);
+        isRightAnswer = false;
     }
 }
-console.log(`Congratulations, ${userName}`);
