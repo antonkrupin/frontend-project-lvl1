fcb489f4ccc02ae353539111fc5fb7012e17ccc6
Anton Krupin <krupin_anton@mail.ru>
2021-08-22 18:08:43 +0300

    add function guessEvenOrNot

--- src/brain-even-game.js
+++ src/brain-even-game.js
@@ -1,3 +1,5 @@
+import takeUserInput from './cli.js';
+
 const getRandom = (max) => Math.floor(Math.random() * max);
 
 const checkUserAnswer = (userAnswer, defaultAnswer = 'yes') => {
@@ -12,4 +14,47 @@ const userWrongAnswerOutput = (userName, userAnswer, correctAnswer = 'no') => {
     console.log(`Let\`s try again, ${userName}`);
 };
 
-export { checkUserAnswer, getRandom, userWrongAnswerOutput };
+const guessEvenOrNot = (userName) => {
+    const endGameCounter = 3;
+    let rightAnswerCounter = 0;
+    let isRightAnswer = true;
+
+    while (isRightAnswer) {
+        const randomNumber = getRandom(1000);
+        console.log(`Question: ${randomNumber}`);
+        const answer = takeUserInput();
+        const divisionRemainder = randomNumber % 2;
+
+        if (answer !== 'yes' && answer !== 'no') {
+            console.log(`${answer} is wrong answer ;(. You need answer 'yes' or 'no'`);
+            console.log(`Let\`s try again, ${userName}`);
+            isRightAnswer = false;
+        }
+
+        if ((checkUserAnswer(answer, 'no') && divisionRemainder === 0)
+            || (checkUserAnswer(answer) && divisionRemainder !== 0)) {
+            if (answer === 'yes') {
+                userWrongAnswerOutput(userName, answer);
+                isRightAnswer = false;
+            } else {
+                userWrongAnswerOutput(userName, answer, 'yes');
+                isRightAnswer = false;
+            }
+        }
+
+        if ((checkUserAnswer(answer) && divisionRemainder === 0)
+            || (checkUserAnswer(answer, 'no') && divisionRemainder !== 0)) {
+            console.log(`Your answer: ${answer}`);
+            console.log('Correct!');
+            rightAnswerCounter += 1;
+            isRightAnswer = true;
+        }
+
+        if (rightAnswerCounter === endGameCounter) {
+            console.log(`Congratulations, ${userName}!`);
+            isRightAnswer = false;
+        }
+    }
+};
+
+export default guessEvenOrNot;
