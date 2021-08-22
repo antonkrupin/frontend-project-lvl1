4f02b3e992a78219bd0c8d8c46d4db0b3a004ae4
Anton Krupin <krupin_anton@mail.ru>
2021-08-22 19:22:17 +0300

    add usage of greeUser function

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,8 @@
 #!/usr/bin/env node
-import takeUserInput from '../src/cli.js';
 import guessEvenOrNot from '../src/brain-even-game.js';
+import greetUser from '../src/index.js';
 
-console.log('Welcome to the Brain Games!');
-const userName = takeUserInput('May I have your name? ');
-console.log(`Hello, ${userName}!`);
+const userName = greetUser();
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
 guessEvenOrNot(userName);
