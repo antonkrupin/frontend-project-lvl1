8369b3d339d097f487137828eaf554ee34fb36a8
Anton Krupin <krupin_anton@mail.ru>
2021-08-30 13:29:25 +0300

    add function userRightAnswerOutput

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import takeUserInput from './cli.js';
 
+// приветствие пользователя
 const greetUser = () => {
     console.log('Welcome to the Brain Games!');
     const userName = takeUserInput('May I have your name? ');
     console.log(`Hello, ${userName}!`);
     return userName;
+};
+
+// вывод сообщения в случае верного ответа
+const userRightAnswerOutput = (answer) => {
+    console.log(`Your answer: ${answer}`);
+    console.log('Correct!');
 };
 
 // вывод сообщения в случае не верного ответа
@@ -13,4 +20,4 @@ const userWrongAnswerOutput = (userName, userAnswer, correctAnswer) => {
     console.log(`Let\`s try again, ${userName}`);
 };
 
-export { greetUser, userWrongAnswerOutput };
+export { greetUser, userRightAnswerOutput, userWrongAnswerOutput };
