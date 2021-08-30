edf39373fab5e4e61fa3143b8176076d4c3f4e29
Anton Krupin <krupin_anton@mail.ru>
2021-08-30 15:47:48 +0300

    import giveExpressionResult

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import { greetUser } from '../src/index.js';
-import giveRightAnswer from '../src/brain-calc-game.js';
+import giveExpressionResult from '../src/brain-calc-game.js';
 
 const userName = greetUser();
 console.log('What is the result of the expression?');
 
-giveRightAnswer(userName);
+giveExpressionResult(userName);
