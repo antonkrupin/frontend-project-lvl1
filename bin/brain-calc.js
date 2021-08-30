7fc226795ffc5b2a54654d083af1041c2f701080
Anton Krupin <krupin_anton@mail.ru>
2021-08-30 13:00:26 +0300

    change import for greetUser

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import greetUser from '../src/index.js';
+import { greetUser } from '../src/index.js';
 import giveRightAnswer from '../src/brain-calc-game.js';
 
 const userName = greetUser();
