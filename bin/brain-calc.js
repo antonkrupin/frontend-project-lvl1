3bd4ad8d26eb37bc61e878287402eaa63a2c3b47
Anton Krupin <krupin_anton@mail.ru>
2021-08-30 20:59:01 +0300

    merge

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -5,8 +5,4 @@ import giveExpressionResult from '../src/brain-calc-game.js';
 const userName = greetUser();
 console.log('What is the result of the expression?');
 
-<<<<<<< HEAD
-// greetUser(userName);
-=======
 giveExpressionResult(userName);
->>>>>>> edf39373fab5e4e61fa3143b8176076d4c3f4e29
