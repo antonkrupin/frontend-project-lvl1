0eec9c36c9cb14122a5ea0c9cb7d3534e20d0f6b
Anton Krupin <krupin_anton@mail.ru>
2021-08-31 09:07:41 +0300

    change giveNODResult function

--- src/brain-gcd-game.js
+++ src/brain-gcd-game.js
@@ -1,8 +1,47 @@
 import takeUserInput from './cli.js';
+import { userRightAnswerOutput, userWrongAnswerOutput, getRandom } from './index.js';
+
+const findNOD = (number1, number2) => {
+    let nod = 1;
+    let nodCounter = 0;
+    if (number1 < number2) {
+        nodCounter = number1;
+    } else {
+        nodCounter = number2;
+    }
+
+    for (let i = 1; i <= nodCounter; i += 1) {
+        if (number1 % i === 0 && number2 % i === 0) {
+            nod = i;
+        }
+    }
+
+    return nod;
+};
 
 const giveNODResult = (userName, endGameCounter = 3) => {
-    const answer = takeUserInput();
-    console.log(answer);
+    let isRightAnswer = true;
+    let rightAnswerCounter = 0;
+
+    while (isRightAnswer) {
+        const firstNumber = getRandom(100);
+        const secondNumber = getRandom(100);
+        console.log(`Question: ${firstNumber} ${secondNumber}`);
+        const answer = Number(takeUserInput());
+
+        if (answer === findNOD(firstNumber, secondNumber)) {
+            userRightAnswerOutput(answer);
+            rightAnswerCounter += 1;
+        } else {
+            userWrongAnswerOutput(userName, answer, findNOD(firstNumber, secondNumber));
+            isRightAnswer = false;
+        }
+
+        if (rightAnswerCounter === endGameCounter) {
+            isRightAnswer = false;
+            console.log(`Congratulations, ${userName}!`);
+        }
+    }
 };
 
 export default giveNODResult;
