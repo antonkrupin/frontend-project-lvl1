5eb3fda274357e6ce8580c77eea2a32fc89e976a
Anton Krupin <krupin_anton@mail.ru>
2021-08-31 08:26:29 +0300

    add giveNODResult function

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,5 +1,8 @@
 #!/usr/bin/env node
+import giveNODResult from '../src/brain-gcd-game.js';
 import { greetUser } from '../src/index.js';
 
 const userName = greetUser();
 console.log('Find the greatest common divisor of given numbers');
+
+giveNODResult(userName);
