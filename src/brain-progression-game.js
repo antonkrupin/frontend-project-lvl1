f4c5373660d423ccfed83d684b9a7d80af2fc995
Anton Krupin <krupin_anton@mail.ru>
2021-09-01 09:32:38 +0300

    add generateProgression function

--- src/brain-progression-game.js
+++ src/brain-progression-game.js
@@ -7,8 +7,21 @@ let isRightAnswer = true;
 let endOfGame = true;
 let rightAnswerCounter = 0;
 
-const completeProgression = (userName, endGameCounter = 3) => {
+const generateProgression = (progressionStep, progressionSize) => {
+    const progresionArray = [];
+    const progressionFirstEl = getRandom(30);
+    progresionArray.push(progressionFirstEl);
+    for (let i = 0; i <= progressionSize; i += 1) {
+        const elForAdd = progresionArray[i] + progressionSize;
+        progresionArray.push(elForAdd);
+    }
+    return progresionArray;
+};
 
+const completeProgression = (userName, endGameCounter = 3) => {
+    while (isRightAnswer && endOfGame) {
+
+    }
 };
 
 export default completeProgression;
