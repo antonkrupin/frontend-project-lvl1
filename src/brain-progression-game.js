a280c840ef301d5725416d730ab8e3fb79d176f5
Anton Krupin <krupin_anton@mail.ru>
2021-09-01 09:25:31 +0300

    add imports from index.js, cli.js. create completeProgression function

---
+++ src/brain-progression-game.js
@@ -0,0 +1,14 @@
+import takeUserInput from './cli.js';
+import {
+    userRightAnswerOutput, userWrongAnswerOutput, getRandom, isGameEnd,
+} from './index.js';
+
+let isRightAnswer = true;
+let endOfGame = true;
+let rightAnswerCounter = 0;
+
+const completeProgression = (userName, endGameCounter = 3) => {
+
+};
+
+export default completeProgression;
