c815f69109e831808fce7cee98f39569be8899de
Anton Krupin <krupin_anton@mail.ru>
2021-09-01 09:50:49 +0300

    change completeProgression function

--- src/brain-progression-game.js
+++ src/brain-progression-game.js
@@ -9,18 +9,33 @@ let rightAnswerCounter = 0;
 
 const generateProgression = (progressionStep, progressionSize) => {
     const progresionArray = [];
-    const progressionFirstEl = getRandom(30);
+    const progressionFirstEl = getRandom(15);
+    const dottedEl = getRandom(progressionSize);
     progresionArray.push(progressionFirstEl);
     for (let i = 0; i <= progressionSize; i += 1) {
-        const elForAdd = progresionArray[i] + progressionSize;
+        const elForAdd = progresionArray[i] + progressionStep;
         progresionArray.push(elForAdd);
     }
-    return progresionArray;
+    const elForGuess = progresionArray[dottedEl];
+    progresionArray[dottedEl] = '..';
+    return [progresionArray, elForGuess];
 };
 
 const completeProgression = (userName, endGameCounter = 3) => {
     while (isRightAnswer && endOfGame) {
+        const step = getRandom(15);
+        const progression = generateProgression(step, 10);
+        console.log(`Question: ${progression[0].join(' ')}`);
+        const answer = Number(takeUserInput());
+        if (answer === progression[1]) {
+            userRightAnswerOutput(answer);
+            rightAnswerCounter += 1;
+        } else {
+            userWrongAnswerOutput(userName, answer, progression[1]);
+            isRightAnswer = false;
+        }
 
+        endOfGame = isGameEnd(rightAnswerCounter, endGameCounter, userName);
     }
 };
 
