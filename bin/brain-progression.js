0ad8a4b46ae1af3e626f7377112a1d79b3263e45
Anton Krupin <krupin_anton@mail.ru>
2021-09-01 09:36:30 +0300

    add usage of completeProgression function

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import { greetUser } from '../src/index.js';
+import completeProgression from '../src/brain-progression-game.js';
 
 const userName = greetUser();
 console.log('What is the result of the expression?');
 console.log(userName);
+completeProgression();
