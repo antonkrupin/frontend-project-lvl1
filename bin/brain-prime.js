6f6ae8e7d23b0dfc8cfe1c13eb9071f94148d873
Anton Krupin <krupin_anton@mail.ru>
2021-09-02 11:31:55 +0300

    change question for user

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -3,6 +3,6 @@ import guessEvenOrNot from '../src/brain-even-game.js';
 import { greetUser } from '../src/index.js';
 
 const userName = greetUser();
-console.log('Answer "yes" if the number is even, otherwise answer "no".');
+console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
 guessEvenOrNot(userName);
