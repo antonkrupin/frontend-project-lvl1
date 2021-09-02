c2925778955638538ce4201f9dbc82b105002ee4
Anton Krupin <krupin_anton@mail.ru>
2021-09-02 11:39:06 +0300

    add import guessPrimeOrNot function

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
-import guessEvenOrNot from '../src/brain-even-game.js';
+import guessPrimeOrNot from '../src/brain-prime-game.js';
 import { greetUser } from '../src/index.js';
 
 const userName = greetUser();
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
 
-guessEvenOrNot(userName);
+guessPrimeOrNot(userName);
