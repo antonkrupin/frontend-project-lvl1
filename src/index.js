7d8b117e2ab6f74c54df1351a9fd592826b9c351
Anton Krupin <krupin_anton@mail.ru>
2021-09-02 13:05:17 +0300

    change indent

--- src/index.js
+++ src/index.js
@@ -2,22 +2,22 @@ import takeUserInput from './cli.js';
 
 // приветствие пользователя
 export const greetUser = () => {
-    console.log('Welcome to the Brain Games!');
-    const userName = takeUserInput('May I have your name? ');
-    console.log(`Hello, ${userName}!`);
-    return userName;
+  console.log('Welcome to the Brain Games!');
+  const userName = takeUserInput('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  return userName;
 };
 
 // вывод сообщения в случае верного ответа
 export const userRightAnswerOutput = (answer) => {
-    console.log(`Your answer: ${answer}`);
-    console.log('Correct!');
+  console.log(`Your answer: ${answer}`);
+  console.log('Correct!');
 };
 
 // вывод сообщения в случае не верного ответа
 export const userWrongAnswerOutput = (userName, userAnswer, correctAnswer) => {
-    console.log(`${userAnswer} is wrong answer ;(. Correct was '${correctAnswer}'`);
-    console.log(`Let's try again, ${userName}!`);
+  console.log(`${userAnswer} is wrong answer ;(. Correct was '${correctAnswer}'`);
+  console.log(`Let's try again, ${userName}!`);
 };
 
 // генерирует случайное целое число
@@ -25,11 +25,11 @@ export const getRandom = (max) => Math.floor(Math.random() * max);
 
 // проверка окончания игры
 export const isGameEnd = (counter, endGameCounter, userName) => {
-    if (counter === endGameCounter) {
-        console.log(`Congratulations, ${userName}!`);
-        return false;
-    }
-    return true;
+  if (counter === endGameCounter) {
+    console.log(`Congratulations, ${userName}!`);
+    return false;
+  }
+  return true;
 };
 
 export default greetUser;
