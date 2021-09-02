29609239d20d1c0e23142104464ee5dc653af682
Anton Krupin <krupin_anton@mail.ru>
2021-09-02 11:19:43 +0300

    create brain-prime.js

---
+++ bin/brain-prime.js
@@ -0,0 +1,8 @@
+#!/usr/bin/env node
+import guessEvenOrNot from '../src/brain-even-game.js';
+import { greetUser } from '../src/index.js';
+
+const userName = greetUser();
+console.log('Answer "yes" if the number is even, otherwise answer "no".');
+
+guessEvenOrNot(userName);
