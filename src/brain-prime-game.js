81c174db756ea20eb54094bba0bc0ede3bd4cf8a
Anton Krupin <krupin_anton@mail.ru>
2021-09-02 11:32:42 +0300

    add guessPrimeOrNot function

--- src/brain-prime-game.js
+++ src/brain-prime-game.js
@@ -2,3 +2,27 @@ import takeUserInput from './cli.js';
 import {
     userRightAnswerOutput, userWrongAnswerOutput, getRandom, isGameEnd,
 } from './index.js';
+
+let isRightAnswer = true;
+let endOfGame = true;
+let rightAnswerCounter = 0;
+
+const guessPrimeOrNot = (userName, endGameCounter = 3) => {
+    while (isRightAnswer && endOfGame) {
+        const step = getRandom(15);
+        const progression = generateProgression(step, 10);
+        console.log(`Question: ${progression[0].join(' ')}`);
+        const answer = Number(takeUserInput());
+        if (answer === progression[1]) {
+            userRightAnswerOutput(answer);
+            rightAnswerCounter += 1;
+        } else {
+            userWrongAnswerOutput(userName, answer, progression[1]);
+            isRightAnswer = false;
+        }
+
+        endOfGame = isGameEnd(rightAnswerCounter, endGameCounter, userName);
+    }
+};
+
+export default guessPrimeOrNot;
