2b7a71ddb5fbc6245bf5297fc169ac2ac8788b4a
Anton Krupin <krupin_anton@mail.ru>
2021-09-07 10:08:24 +0300

    add usage of startGame function

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-import giveNODResult from '../src/brain-gcd-game.js';
-import { greetUser } from '../src/index.js';
+import startGame, { greetUser, giveNODResult } from '../src/index.js';
 
 const userName = greetUser();
 console.log('Find the greatest common divisor of given numbers');
 
-giveNODResult(userName);
+startGame(userName, giveNODResult);
