f5769144d0f1394da5fb4b89a48f39b1440cc299
Anton Krupin <krupin_anton@mail.ru>
2021-09-07 10:08:05 +0300

    add startGame function

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import takeUserInput from './cli.js';
+import findNOD from './brain-gcd-game.js';
 
 // приветствие пользователя
 export const greetUser = () => {
@@ -32,4 +33,37 @@ export const isGameEnd = (counter, endGameCounter, userName) => {
   return true;
 };
 
-export default greetUser;
+export const giveNODResult = (userName) => {
+  const firstNumber = getRandom(100);
+  const secondNumber = getRandom(100);
+  console.log(`Question: ${firstNumber} ${secondNumber}`);
+  const answer = Number(takeUserInput());
+  const result = findNOD(firstNumber, secondNumber);
+  if (answer === result) {
+    userRightAnswerOutput(answer);
+    return true;
+  }
+  userWrongAnswerOutput(userName, answer, result);
+  return false;
+};
+
+const startGame = (userName, gameFunction, endGameCounter = 3) => {
+  let rightAnswerCounter = 0;
+  let isRightAnswer = true;
+
+  while (isRightAnswer) {
+    const result = gameFunction(userName);
+    if (result) {
+      rightAnswerCounter += 1;
+    } else {
+      isRightAnswer = false;
+    }
+
+    if (rightAnswerCounter === endGameCounter) {
+      console.log(`Congratulations, ${userName}!`);
+      isRightAnswer = false;
+    }
+  }
+};
+
+export default startGame;
