43b2814040ad30814d0408aeb1800aaf6a6ca075
Anton Krupin <krupin_anton@mail.ru>
2021-09-07 11:52:55 +0300

    remove comments

--- src/brain-even-game.js
+++ src/brain-even-game.js
@@ -1,17 +1,6 @@
 // eslint-disable-next-line import/no-cycle
 import { userRightAnswerOutput, userWrongAnswerOutput } from './index.js';
 
-// проверка корректности ввода ответа
-/* const isCorrectAnswer = (answer, userName) => {
-  if (answer !== 'yes' && answer !== 'no') {
-    console.log(`${answer} is wrong answer ;(. You need answer 'yes' or 'no'`);
-    console.log(`Let\`s try again, ${userName}`);
-    isRightAnswer = false;
-    return false;
-  }
-  return true;
-}; */
-
 // проверка остатка от деления
 const isZeroDivision = (divisionRemainder, answer, userName) => {
   if (divisionRemainder === 0 && answer === 'yes') {
@@ -28,44 +17,5 @@ const isZeroDivision = (divisionRemainder, answer, userName) => {
   }
   return false;
 };
-
-/*
-// проверка остатка от деления
-const isZeroDivision = (divisionRemainder, answer, userName) => {
-  if (divisionRemainder === 0 && answer === 'yes') {
-    userRightAnswerOutput(answer);
-    rightAnswerCounter += 1;
-  } else if (divisionRemainder !== 0 && answer === 'no') {
-    userRightAnswerOutput(answer);
-    rightAnswerCounter += 1;
-  } else {
-    if (answer === 'yes') {
-      userWrongAnswerOutput(userName, answer, 'no');
-    } else {
-      userWrongAnswerOutput(userName, answer, 'yes');
-    }
-    isRightAnswer = false;
-  }
-};
-
-*/
-
-/*
-const guessEvenOrNot = (userName, endGameCounter = 3) => {
-  while (isRightAnswer && endOfGame) {
-    const randomNumber = getRandom(1000);
-    console.log(`Question: ${randomNumber}`);
-    const answer = takeUserInput();
-    const divisionRemainder = randomNumber % 2;
-
-    if (!isCorrectAnswer(answer, userName)) {
-      break;
-    }
-
-    isZeroDivision(divisionRemainder, answer, userName);
-
-    endOfGame = isGameEnd(rightAnswerCounter, endGameCounter, userName);
-  }
-}; */
 
 export default isZeroDivision;
