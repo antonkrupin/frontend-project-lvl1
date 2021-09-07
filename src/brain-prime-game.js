4bedaefff491a549bc8397e19a0202e9f00556a5
Anton Krupin <krupin_anton@mail.ru>
2021-09-07 11:54:04 +0300

    remove comments

--- src/brain-prime-game.js
+++ src/brain-prime-game.js
@@ -7,22 +7,4 @@ const isPrime = (number) => {
   return 'yes';
 };
 
-/* const guessPrimeOrNot = (userName, endGameCounter = 3) => {
-  while (isRightAnswer && endOfGame) {
-    const number = getRandom(100);
-    console.log(`Question: ${number}`);
-    const answer = takeUserInput();
-    const rightAnswer = isPrime(number);
-    if (answer === rightAnswer) {
-      userRightAnswerOutput(answer);
-      rightAnswerCounter += 1;
-    } else {
-      userWrongAnswerOutput(userName, answer, rightAnswer);
-      isRightAnswer = false;
-    }
-
-    endOfGame = isGameEnd(rightAnswerCounter, endGameCounter, userName);
-  }
-}; */
-
 export default isPrime;
