26ea00a5ca946347786e66b17cfe210d8d3da360
Anton Krupin <krupin_anton@mail.ru>
2021-09-07 21:27:10 +0300

    remove isZeroDivision function

--- src/brain-even-game.js
+++ src/brain-even-game.js
@@ -1,23 +1,6 @@
 import { getRandom } from './index.js';
 import takeUserInput from './cli.js';
 
-// проверка остатка от деления
-/* const isZeroDivision = (divisionRemainder, answer, userName) => {
-  if (divisionRemainder === 0 && answer === 'yes') {
-    userRightAnswerOutput(answer);
-    return true;
-  } if (divisionRemainder !== 0 && answer === 'no') {
-    userRightAnswerOutput(answer);
-    return true;
-  }
-  if (answer === 'yes') {
-    userWrongAnswerOutput(userName, answer, 'no');
-  } else {
-    userWrongAnswerOutput(userName, answer, 'yes');
-  }
-  return false;
-}; */
-
 const guessEvenOrNot = () => {
   const randomNumber = getRandom(1000);
   console.log(`Question: ${randomNumber}`);
