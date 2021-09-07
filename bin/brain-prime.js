8ac7f83fa8fb98c64b2d0f2db1734409ed5ee254
Anton Krupin <krupin_anton@mail.ru>
2021-09-07 12:19:19 +0300

    change import to cli.js

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import startGame, { greetUser, guessPrimeOrNot } from '../src/index.js';
+import { greetUser } from '../src/cli.js';
+import startGame, { guessPrimeOrNot } from '../src/index.js';
 
 const userName = greetUser();
 console.log('Answer "yes" if given number is prime. Otherwise answer "no".');
