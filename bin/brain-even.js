b786f74e976d02f236cda45aa6e7365f91124220
Anton Krupin <krupin_anton@mail.ru>
2021-09-07 11:49:17 +0300

    add usage of startGame function

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-import guessEvenOrNot from '../src/brain-even-game.js';
-import { greetUser } from '../src/index.js';
+import startGame, { greetUser, guessEvenOrNot } from '../src/index.js';
 
 const userName = greetUser();
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-guessEvenOrNot(userName);
+startGame(userName, guessEvenOrNot);
