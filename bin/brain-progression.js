0eda4f3dad87db60097b01be6149734ea5ab259a
Anton Krupin <krupin_anton@mail.ru>
2021-09-07 12:19:30 +0300

    change import to cli.js

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import startGame, { completeProgression, greetUser } from '../src/index.js';
+import { greetUser } from '../src/cli.js';
+import startGame, { completeProgression } from '../src/index.js';
 
 const userName = greetUser();
 console.log('What is the result of the expression?');
