07c1f78c452b4f63d6d5e73a9bd919bac5f74dc1
Anton Krupin <krupin_anton@mail.ru>
2021-09-07 12:20:06 +0300

    add greetUser, userRightAnswerOutput, userWrongAnswerOutput functions

--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,29 @@
 import readlineSync from 'readline-sync';
 
+// считывание ввода пользователя
 const takeUserInput = (userQuestion) => {
   const input = readlineSync.question(userQuestion);
   return input;
+};
+
+// приветствие пользователя
+export const greetUser = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = takeUserInput('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
+  return userName;
+};
+
+// вывод сообщения в случае верного ответа
+export const userRightAnswerOutput = (answer) => {
+  console.log(`Your answer: ${answer}`);
+  console.log('Correct!');
+};
+
+// вывод сообщения в случае не верного ответа
+export const userWrongAnswerOutput = (userName, userAnswer, correctAnswer) => {
+  console.log(`${userAnswer} is wrong answer ;(. Correct was '${correctAnswer}'`);
+  console.log(`Let's try again, ${userName}!`);
 };
 
 export default takeUserInput;
