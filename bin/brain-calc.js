1e99554ea6fd41020b45a2965b0ed83a0455723d
Anton Krupin <krupin_anton@mail.ru>
2021-09-07 10:50:19 +0300

    add usage of startGame function

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-import { greetUser } from '../src/index.js';
-import giveExpressionResult from '../src/brain-calc-game.js';
+import startGame, { greetUser, giveExpressionResult } from '../src/index.js';
 
 const userName = greetUser();
 console.log('What is the result of the expression?');
 
-giveExpressionResult(userName);
+startGame(userName, giveExpressionResult);
