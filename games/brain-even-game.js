d92a302053f196595d8b1792671a5fcdeb972c56
Anton Krupin <krupin_anton@mail.ru>
2021-09-09 19:41:06 +0300

    change guessEvenOrNot function

--- games/brain-even-game.js
+++ games/brain-even-game.js
@@ -1,13 +1,14 @@
-import takeUserInput from '../src/cli.js';
+// import takeUserInput from '../src/cli.js';
 import getRandom from '../src/asset-functions.js';
 
 const guessEvenOrNot = () => {
   const randomNumber = getRandom(1000);
-  console.log(`Question: ${randomNumber}`);
-  const answer = takeUserInput();
+  const question = randomNumber;
+  // console.log(`Question: ${randomNumber}`);
+  // const answer = takeUserInput();
   const divisionRemainder = randomNumber % 2;
   const result = divisionRemainder === 0 ? 'yes' : 'no';
-  return [answer, result];
+  return [question, result];
 };
 
 export default guessEvenOrNot;
