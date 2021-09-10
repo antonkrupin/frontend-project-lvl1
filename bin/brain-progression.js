8bcbb6719ce5319af07ad40fed387233b07cdcca
Anton Krupin <krupin_anton@mail.ru>
2021-09-10 09:15:42 +0300

    remove commented imports

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-// import startGame from '../src/index.js';
-// import completeProgression from '../games/brain-progression-game.js';
-
 import { startFuckingGame } from '../src/asset-functions.js';
 
 startFuckingGame('completeProgression', 'What is the result of the expression?');
-
-// startGame(completeProgression, 'What is the result of the expression?');
