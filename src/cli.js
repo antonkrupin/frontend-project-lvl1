3b87f86b4c7b5406fa9a1d6e16ce0b98155509a8
Anton Krupin <krupin_anton@mail.ru>
2021-09-10 09:30:03 +0300

    remove greetUser, userRightAnswer, userWrongAnswer function

--- src/cli.js
+++ src/cli.js
@@ -6,24 +6,4 @@ const takeUserInput = (userQuestion) => {
   return input;
 };
 
-// приветствие пользователя (то что оставить)
-export const greetUser = () => {
-  console.log('Welcome to the Brain Games!');
-  const userName = takeUserInput('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  return userName;
-};
-
-// вывод сообщения в случае верного ответа
-export const userRightAnswerOutput = (answer) => {
-  console.log(`Your answer: ${answer}`);
-  console.log('Correct!');
-};
-
-// вывод сообщения в случае не верного ответа
-export const userWrongAnswerOutput = (userName, userAnswer, correctAnswer) => {
-  console.log(`${userAnswer} is wrong answer ;(. Correct was '${correctAnswer}'`);
-  console.log(`Let's try again, ${userName}!`);
-};
-
 export default takeUserInput;
