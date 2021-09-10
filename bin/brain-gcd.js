4c0ef6966e1c0ffac3e88a6d87bcf5d37d7c9506
Anton Krupin <krupin_anton@mail.ru>
2021-09-10 09:15:27 +0300

    remove commented imports

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
-// import startGame from '../src/index.js';
-// import giveNODResult from '../games/brain-gcd-game.js';
 import { startFuckingGame } from '../src/asset-functions.js';
 
 startFuckingGame('giveNODResult', 'Find the greatest common divisor of given numbers');
-
-// startGame(giveNODResult, 'Find the greatest common divisor of given numbers');
