5f92e9961fb6c11bebe906734161ae7087ee49bb
Anton Krupin <krupin_anton@mail.ru>
2021-09-10 09:15:18 +0300

    remove commented imports

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,9 +1,4 @@
 #!/usr/bin/env node
-// import startGame from '../src/index.js';
-// import guessEvenOrNot from '../games/brain-even-game.js';
-
 import { startFuckingGame } from '../src/asset-functions.js';
 
 startFuckingGame('guessEvenOrNot', 'Answer "yes" if the number is even, otherwise answer "no".');
-
-// startGame(guessEvenOrNot, 'Answer "yes" if the number is even, otherwise answer "no".');
