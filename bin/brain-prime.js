e19e317e505b32648da662af917f302100401025
Anton Krupin <krupin_anton@mail.ru>
2021-09-10 09:15:35 +0300

    remove commented imports

--- bin/brain-prime.js
+++ bin/brain-prime.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
-// import startGame from '../src/index.js';
-// import guessPrimeOrNot from '../games/brain-prime-game.js';
 import { startFuckingGame } from '../src/asset-functions.js';
 
 startFuckingGame('guessPrimeOrNot', 'Answer "yes" if given number is prime. Otherwise answer "no".');
-
-// startGame(guessPrimeOrNot, 'Answer "yes" if given number is prime. Otherwise answer "no".');
