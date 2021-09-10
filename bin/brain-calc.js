1879e5786eebcfccfd857a86b49ab14832bfd012
Anton Krupin <krupin_anton@mail.ru>
2021-09-10 09:15:09 +0300

    remove commented imports

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,7 +1,4 @@
 #!/usr/bin/env node
 import { startFuckingGame } from '../src/asset-functions.js';
-// import startGame from '../src/index.js';
-// import giveExpressionResult from '../games/brain-calc-game.js';
 
 startFuckingGame('giveExpressionResult', 'What is the result of the expression?');
-// startGame(giveExpressionResult, 'What is the result of the expression?');
