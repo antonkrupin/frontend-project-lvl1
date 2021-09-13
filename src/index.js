fdce7d9bf179cfaa1e184d93d28907d579d79276
Anton Krupin <krupin_anton@mail.ru>
2021-09-13 11:07:13 +0300

    remove comment eslint no-cycle

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import readlineSync from 'readline-sync';
-// eslint-disable-next-line import/no-cycle
 import { userRightAnswerOutput, userWrongAnswerOutput } from './asset-functions.js';
 
 const gameRoundCounter = 3;
