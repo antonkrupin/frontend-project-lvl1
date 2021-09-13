394eabb62b307e2855df208e8da01f58eaf72a22
Anton Krupin <krupin_anton@mail.ru>
2021-09-13 10:29:10 +0300

    rename dottedElIndex to hiddenIndex

--- src/games/brain-progression-game.js
+++ src/games/brain-progression-game.js
@@ -7,14 +7,14 @@ import mainGameFunction from '../index.js';
 const generateProgression = (progressionStep, progressionSize) => {
   const progresionArray = [];
   const progressionFirstEl = getRandom(15);
-  const dottedElIndex = getRandom(progressionSize);
+  const hiddenIndex = getRandom(progressionSize);
   progresionArray.push(progressionFirstEl);
   for (let i = 0; i <= progressionSize; i += 1) {
     const progressionEl = progresionArray[i] + progressionStep;
     progresionArray.push(progressionEl);
   }
-  const elForGuess = progresionArray[dottedElIndex];
-  progresionArray[dottedElIndex] = '..';
+  const elForGuess = progresionArray[hiddenIndex];
+  progresionArray[hiddenIndex] = '..';
   return [progresionArray, elForGuess];
 };
 
