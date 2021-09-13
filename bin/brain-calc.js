6a5d0ff598746ef2d1095753ac1d3767a9d91f96
Anton Krupin <krupin_anton@mail.ru>
2021-09-13 09:33:00 +0300

    remove comments

--- bin/brain-calc.js
+++ bin/brain-calc.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-// import { mainStartGame } from '../src/asset-functions.js';
 import startGame from '../src/games/brain-calc-game.js';
 
 startGame();
-// mainStartGame('giveExpressionResult', 'What is the result of the expression?');
