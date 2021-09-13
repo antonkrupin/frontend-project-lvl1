52376b129aeba5fb80ac27ede04f0feeeb982fc2
Anton Krupin <krupin_anton@mail.ru>
2021-09-13 10:24:34 +0300

    rename findNOD to findGCD, rename giveNODResult to generateRound

--- src/games/brain-gcd-game.js
+++ src/games/brain-gcd-game.js
@@ -4,25 +4,25 @@ import getRandom from '../asset-functions.js';
 import mainGameFunction from '../index.js';
 
 // нахождение наибольшего общего делителя
-const findNOD = (number1, number2) => {
+const findGCD = (number1, number2) => {
   if (number2 === 0) {
     return Math.abs(number1);
   }
-  return findNOD(number2, number1 % number2);
+  return findGCD(number2, number1 % number2);
 };
 
-const giveNODResult = () => {
+const generateRound = () => {
   const firstNumber = getRandom(100);
   const secondNumber = getRandom(100);
   const question = [firstNumber, secondNumber];
-  const result = findNOD(firstNumber, secondNumber);
+  const result = findGCD(firstNumber, secondNumber);
   return [question, String(result)];
 };
 
 const gameRules = 'Find the greatest common divisor of given numbers';
 
 const startGame = () => {
-  mainGameFunction(giveNODResult, gameRules);
+  mainGameFunction(generateRound, gameRules);
 };
 
 export default startGame;
