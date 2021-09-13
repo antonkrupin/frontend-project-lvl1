f96aa419fd06578b734490dd9a45475c3543a584
Anton Krupin <krupin_anton@mail.ru>
2021-09-13 11:05:23 +0300

    remove getRandom function

--- src/asset-functions.js
+++ src/asset-functions.js
@@ -9,7 +9,3 @@ export const userWrongAnswerOutput = (userName, userAnswer, correctAnswer) => {
   console.log(`${userAnswer} is wrong answer ;(. Correct was '${correctAnswer}'`);
   console.log(`Let's try again, ${userName}!`);
 };
-
-const getRandom = (max) => Math.floor(Math.random() * max);
-
-export default getRandom;
