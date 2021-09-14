a030fa10029b038902439c0c5696329f7926f947
Anton Krupin <krupin_anton@mail.ru>
2021-09-14 16:03:21 +0300

    add length - 1

--- src/games/brain-calc-game.js
+++ src/games/brain-calc-game.js
@@ -13,7 +13,7 @@ const calculateExpression = (firstNumber, secondNumber, expressionSign) => {
 
 const generateRound = () => {
   const expressionsSignArray = ['+', '-', '*'];
-  const expressionNumber = getRandom(expressionsSignArray.length);
+  const expressionNumber = getRandom(expressionsSignArray.length - 1);
   const firstNumber = getRandom(100);
   const secondNumber = getRandom(100);
   const sign = expressionsSignArray[expressionNumber];
