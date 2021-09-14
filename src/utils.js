0420db995ae09826eab5321bf3e4316e733fb5d9
Anton Krupin <krupin_anton@mail.ru>
2021-09-14 16:08:58 +0300

    add comment

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 // не смог понять, как сделать так
 // чтобы максимальное значение тоже попадало
 // при любых моих решениях в brain-calc вылезали ошибки
+// comment
 const getRandom = (max, min = 0) => Math.floor(Math.random() * (max - min + 1)) + min;
 
 export default getRandom;
