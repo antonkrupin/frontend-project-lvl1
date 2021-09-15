4b5704b30591b4a08a462d5cf9141f38ea83dee9
Anton Krupin <krupin_anton@mail.ru>
2021-09-15 14:59:32 +0300

    remove return UserName

--- src/cli.js
+++ src/cli.js
@@ -4,7 +4,6 @@ import readlineSync from 'readline-sync';
 const greetUser = () => {
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
-  return userName;
 };
 
 export default greetUser;
