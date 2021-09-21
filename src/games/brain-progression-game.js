ff46d8c06a17b8bf5f1b92b372cc9bc19022e2f4
Anton Krupin <krupin_anton@mail.ru>
2021-09-21 14:32:52 +0300

    remove =

--- src/games/brain-progression-game.js
+++ src/games/brain-progression-game.js
@@ -6,7 +6,7 @@ const gameRules = 'What is the result of the expression?';
 const generateProgression = (progressionStep, progressionSize, firstElement, hiddenIndex) => {
   const progresionArray = [];
   progresionArray.push(firstElement);
-  for (let i = 0; i <= progressionSize; i += 1) {
+  for (let i = 0; i < progressionSize; i += 1) {
     const progressionEl = progresionArray[i] + progressionStep;
     progresionArray.push(progressionEl);
   }
