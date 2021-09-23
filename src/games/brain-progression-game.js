f428b70e967b3d9c7ac109c3eed127638e5374fb
Anton Krupin <krupin_anton@mail.ru>
2021-09-23 07:10:21 +0300

    in generatProgression function change for loop - starts from 0

--- src/games/brain-progression-game.js
+++ src/games/brain-progression-game.js
@@ -5,8 +5,7 @@ const gameRules = 'What is the result of the expression?';
 
 const generateProgression = (progressionStep, progressionSize, firstElement, hiddenIndex) => {
   const progresionArray = [];
-  progresionArray.push(firstElement);
-  for (let i = 1; i < progressionSize; i += 1) {
+  for (let i = 0; i < progressionSize; i += 1) {
     const progressionEl = firstElement + progressionStep * i;
     progresionArray.push(progressionEl);
   }
